0f8d261383d8877c70fcdfb931e593d2b65ef5cd
Semy Levy <semylevy@hotmail.com>
2018-02-15 01:43:21 -0600

    First release

--- Actividades/A5.1.osbert/galeria.h
+++ Actividades/A5.1.osbert/galeria.h
@@ -14,64 +14,108 @@ private:
     string direccion;
 public:
     Cliente(string nombre_in, string direccion_in): nombre(nombre_in), direccion(direccion_in) { }
+    string getNombre() {
+        return nombre;
+    }
 };
 
 class Cuadro {
 private:
     string autor;
+    string nombre;
     float precio_inicial;
 public:
-    Cuadro(string autor_in, float precio): autor(autor_in), precio_inicial(precio) { }
+    Cuadro(string autor_in, string nombre_in, float precio): autor(autor_in), nombre(nombre_in), precio_inicial(precio) { }
+    string getAutor() {
+        return autor;
+    }
+    string getNombre() {
+        return nombre;
+    }
+    float getPrecio() {
+        return precio_inicial;
+    }
 };
 
 class Venta {
 private:
     float precio;
-    int fecha;
-    Cuadro *pintura;
-    Cliente *comprador;
+    int year;
+    Cuadro pintura;
+    Cliente comprador;
 public:
-    Venta(Cuadro *pintura_in, Cliente *comprador_in, float precio_in): pintura(pintura_in), comprador(comprador_in), precio(precio_in) {
-        // Agregar formato fecha
-        fecha = 1;
+    Venta(Cuadro pintura_in, Cliente comprador_in, float precio_in, int year_in=0): pintura(pintura_in), comprador(comprador_in), precio(precio_in), year(year_in) {
+        if(!year) {
+            time_t now = time(0);
+            tm *fecha = localtime(&now);
+            year = fecha->tm_year+1900;
+        }
     }
-    float generaPrecio() {
-        return 1;
+    void imprimir() {
+        cout << pintura.getAutor() << " - " << pintura.getNombre() << ". Comprado por " << comprador.getNombre() << ", " << year <<". \n";
+    }
+    string getAutor() {
+        return pintura.getAutor();
+    }
+    int getYear() {
+        return year;
+    }
+    float getPrecio() {
+        return precio;
     }
 };
 
 class Ventas {
 private:
-    vector<Venta> *lista_ventas;
+    vector<Venta> lista_ventas;
 public:
     Ventas() { }
-    Venta getSimilares(Cuadro *pieza) {
-        return lista_ventas.;
+    Venta getSimilares(Cuadro pieza) {
+        for(int i = 0; i < lista_ventas.size(); i++) {
+            if(lista_ventas[i].getAutor() == pieza.getAutor())
+                return lista_ventas[i];
+        }
+        return Venta(Cuadro("","",-1), Cliente("",""), -1);
     }
-    void addVenta(Venta nueva_venta) {
-        //lista_ventas->insert(nueva_venta);
+    void imprimir() {
+        for (int i = 0; i < lista_ventas.size(); i++)
+            lista_ventas[i].imprimir();
+    }
+    void nuevaVenta(Venta nueva) {
+        lista_ventas.push_back(nueva);
     }
 };
 
 class Galeria {
 private:
-    Ventas *historial;
+    Ventas historial;
 public:
-    Galeria() {
-        //historial = new Ventas;
+    Galeria() { }
+    float getOferta(Cuadro pieza) {
+        Venta similar = historial.getSimilares(pieza);
+        if(similar.getPrecio() == -1) {
+            return -1;
+        }
+        return calculaPrecio(similar);
     }
-    float getOferta(Cuadro *pieza) {
-        //Cuadro similar = historial->getSimilares(pieza);
-        return calculaPrecio(pieza);
-    }
-    bool vender(Cuadro *pieza, Cliente *comprador, float precio) {
-        //Venta *transaccion = new Venta(pieza, comprador, precio);
-        //historial->addVenta(transaccion);
+    bool vender(Cuadro pieza, Cliente comprador, float precio, int year=0) {
+        Venta transaccion(pieza, comprador, precio, year);
+        historial.nuevaVenta(transaccion);
         return true;
     }
-    float calculaPrecio(Cuadro *pieza) {
-        // Agregar cálculo de precio
-        return 100;
+    float calculaPrecio(Venta pieza) {
+        time_t now = time(0);
+        tm *ltm = localtime(&now);
+        int curr_year = ltm->tm_year + 1900;
+        int years = curr_year - pieza.getYear();
+        float precio = pieza.getPrecio();
+        for(int i = 0; i < years; i++) {
+            precio*=1.085;
+        }
+        return precio;
+    }
+    void imprimirVentas() {
+        historial.imprimir();
     }
 };
 
--- Actividades/A5.1.osbert/main.cpp
+++ Actividades/A5.1.osbert/main.cpp
@@ -2,7 +2,46 @@
 #include "galeria.h"
 
 int main() {
+    Galeria mi_tienda;
+    Cliente cli_1("Semy", "Dir");
+    Cuadro cuad_1("Picasso", "Guernica", 100);
+    Cuadro cuad_2("Frida Kahlo", "Diego y yo", 50);
+    mi_tienda.vender(cuad_1, cli_1, 120, 1980);
+    mi_tienda.vender(cuad_2, cli_1, 80, 1995);
     
+    string main_input;
     
+    do {
+        cout << "1 -- Obtener oferta\n2 -- Imprimir ventas\n3 -- Ayuda\n0 -- Salir\n";
+        getline (cin, main_input);
+        if(main_input == "1") {
+            string nombre, autor;
+            cout << "Nombre del cuadro: ";
+            getline (cin, nombre);
+            cout << "Autor del cuadro: ";
+            getline (cin, autor);
+            float precio = mi_tienda.getOferta(Cuadro(autor, nombre, 0));
+            if(precio == -1) {
+                cout << "No se encontró ningún cuadro similar.\n";
+                continue;
+            }
+            cout << "Precio del cuadro: " << precio << "\n¿Comprar por ese precio?\n[y] Si\n[n] No\n";
+            string purchase_answer;
+            getline (cin, purchase_answer);
+            if(purchase_answer == "y") {
+                string nombre_comprador, direccion_comprador;
+                cout << "Nombre del comprador: ";
+                getline (cin, nombre_comprador);
+                cout << "Dirección del comprador: ";
+                getline (cin, direccion_comprador);
+                mi_tienda.vender(Cuadro(autor, nombre, precio),Cliente(nombre_comprador, direccion_comprador), precio);
+                cout << "¡Gracias por comprar!\n";
+            }
+        } else if(main_input == "2") {
+            mi_tienda.imprimirVentas();
+        } else if(main_input == "3") {
+            cout << "\nInstrucciones:\nPara agregar ventas anteriores, editar directamente el código. El formato es el siguiente:\nLlamar método Galeria.vender(Cuadro, Cliente, Precio, Año) con objetos:\n\n Cuadro(Autor, Título, Precio)\n Cliente(Nombre, Dirección)\n\nSemy Levy, 2018.\n\n";
+        }
+    } while(main_input != "0");
     return 1;
 }
